1ca3e73e8239696777a708be04ca5d16b6bf62ee
AvirSaam <den.demchenko@gmail.com>
2023-02-11 17:31:47 +0300

    task 66 completed

--- task64/Program.cs
+++ task64/Program.cs
@@ -5,7 +5,7 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
  */
 
-void decrementN (int N){        
+void DecrementN (int N){        
     Console.Write($"{N--}, ");
 
     if (N != 0) decrementN(N);
---
+++ task66/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+int SumOfRange (int rangeStart, int rangeEnd)
+{
+    if (rangeStart == rangeEnd) return rangeEnd;
+    else
+    {
+        if (rangeStart < rangeEnd) return rangeStart + SumOfRange(++rangeStart, rangeEnd);
+        else return rangeStart + SumOfRange(--rangeStart, rangeEnd);
+    }
+}
+
+const int M = 4, N = 8;
+Console.Clear();
+Console.WriteLine ($"Sum of all numbers in range berween {M} and {N} is {SumOfRange(M, N)}");
+
